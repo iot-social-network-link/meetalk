1486e14c700a41428b4e78ce4b14e12b0c972a94
mats0228 <m.hisa3153@gmail.com>
2015-12-23 01:40:46 +0900

    video_chat.jsの整理とexit機能修正

--- app/assets/javascripts/video_chat.js
+++ app/assets/javascripts/video_chat.js
@@ -1,19 +1,9 @@
-function video_chat_start(s_room_id, a_nickname) {
-    // var multiparty;
+/* -----------------------------------------------------------------
+ * Skyway multyparty 利用したライブラリ
+ *  
+ * ----------------------------------------------------------------- */
 
-    //debug
-    alert('roomid:  '+ s_room_id);
-    alert('nickname1:  '+ a_nickname[1] );
-
-    // MultiParty インスタンスを生成
-    multiparty = new MultiParty( {
-	    "key": "44ed614d-25eb-4a1f-b7a8-a47acd9f7595",
-	    "reliable": true,
-	    "debug": 3
-	    //"room_id": room_id
-	});
-    /////////////////////////////////
-    // for MediaStream
+function manage_mediasteam(s_roomid, s_nickname) {
     multiparty.on('my_ms', function(video) {
 	    // 自分のvideoを表示
 	    var vNode = MultiParty.util.createVideoNode(video);
@@ -21,31 +11,29 @@ function video_chat_start(s_room_id, a_nickname) {
 	    vNode.volume = 0;
 	    $(vNode).appendTo("#streams");
 	}).on('peer_ms', function(video) {
-		console.log("video received!!")
-		    // peerのvideoを表示
-		    console.log(video);
-		var vNode = MultiParty.util.createVideoNode(video);
-		vNode.setAttribute("class", "video peer-video");
-		$(vNode).appendTo("#streams");
-		console.log($("#streams"))
-		    }).on('ms_close', function(peer_id) {
-			    // peerが切れたら、対象のvideoノードを削除する
-			    $("#"+peer_id).remove();
-			})
-	////////////////////////////////
-	// for DataChannel
-	multiparty.on('message', function(mesg) {
-		// peerからテキストメッセージを受信
-		$("p.receive").append(mesg.data + "<br>");
-	    });
-    ////////////////////////////////
-    // Error handling
+	    console.log("video received!!")
+	    // peerのvideoを表示
+	    var vNode = MultiParty.util.createVideoNode(video);
+	    vNode.setAttribute("class", "video peer-video");
+	    $(vNode).appendTo("#streams");
+	    console.log($("#streams"))
+	}).on('ms_close', function(peer_id) {
+	    // peerが切れたら、対象のvideoノードを削除する
+	    $("#"+peer_id).remove();
+	})
+	
+    // Error handling:★ユーザに何を表示すべきか要検討
     multiparty.on('error', function(err) {
 	    alert(err);
 	});
-    multiparty.start();
-    //////////////////////////////////////////////////////////
-    // テキストフォームに入力されたテキストをpeerに送信
+}
+
+function manage_message(){
+    multiparty.on('message', function(mesg) {
+	    // peerからテキストメッセージを受信
+	    $("p.receive").append(mesg.data + "<br>");
+	});
+
     $("#message form").on("submit", function(ev) {
 	    ev.preventDefault();  // onsubmitのデフォルト動作（reload）を抑制
 	    // テキストデータ取得
@@ -61,7 +49,24 @@ function video_chat_start(s_room_id, a_nickname) {
 	});
 }
 
-function exit_video_chat(){
-  alert("Exit!!");
-  multiparty.close();
+//先に、manage_***()の定義が必要
+function video_chat_start(s_roomid, s_nickname) {
+    multiparty = new MultiParty( {
+	    "key": "44ed614d-25eb-4a1f-b7a8-a47acd9f7595",
+	    "reliable": true,
+	    "room_id": s_roomid,
+	    "debug": 3
+	});
+    manage_message();
+    // サーバとpeerに接続
+    multiparty.start();
+    manage_mediasteam(s_roomid, s_nickname);
 }
+function exit_video_chat(){
+    console.log("Exit!!");
+    multiparty.close();
+    var top_url = "http://" + location.host;
+    console.log(top_url);
+    location.href=top_url; //redirect to top.
+}
+
