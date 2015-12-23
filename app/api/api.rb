1c38b64185cf3b9d4234b22ad64fc39d0b9dbd3e
xkumiyu <xkumiyu@gmail.com>
2015-12-24 00:07:11 +0900

    リファクタリング

--- app/api/api.rb
+++ app/api/api.rb
@@ -4,26 +4,24 @@ class API < Grape::API
   version 'v1', :using => :path
 
   resource "users" do
-# All User Info API: http://localhost:3000/api/v1/users
+    # All User Info API: http://localhost:3000/api/v1/users
     desc "returns all users"
     get do
       User.all
     end
 
-# Users in the room API: http://localhost:3000/api/v1/users/$room_id
+    # Users in the room API: http://localhost:3000/api/v1/users/:room_id
     desc "return a user in the room"
     params do
       requires :room_id, type: Integer
     end
     get ':room_id' do
-      User.where(room_id:  params[:room_id])
+      User.where(room_id: params[:room_id])
     end
   end
 
-
-
   resource "rooms" do
-# All Rooms Info API: http://localhost:3000/api/v1/rooms
+    # All Rooms Info API: http://localhost:3000/api/v1/rooms
 	  desc "returns all rooms"
 	  get do
 		  Room.all
@@ -31,20 +29,19 @@ class API < Grape::API
   end
 
   resource "room_full" do
-
-# Room Full API: http://localhost:3000/api/v1/full_room/$room_id
-	  desc "return True when four member joined in the rooom"
-	    params do
-      		requires :id, type: Integer
-    	end
- 	  get ':id' do
-		room = Room.find(params[:id])
-		if room.male + room.female == 4
-			{"result" => 'True'}
-		else 
-			{"result" => 'False'}
-		end
+    # Room Full API: http://localhost:3000/api/v1/room_full/:room_id
+	  desc "return True when four member joined in the room"
+    params do
+      requires :room_id, type: Integer
+    end
+ 	  get ':room_id' do
+      room = Room.find(params[:room_id])
+      if (room.male + room.female) == 4
+        { result: true }
+      else
+        { result: false }
+      end
 	  end
-
   end
+
 end
