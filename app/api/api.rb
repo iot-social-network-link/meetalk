bad2a0bf8f1b21e74d3a723cfd284e61997b3f79
Hiro <megaroom.dev@gmail.com>
2015-12-26 17:17:03 +0900

    API created to get user information with window_id and room_id

--- app/api/api.rb
+++ app/api/api.rb
@@ -4,11 +4,12 @@ class API < Grape::API
   version 'v1', :using => :path
 
   resource "user" do
+
+  desc "Register window_id to User"
 	  # Register window_id on User Table: http://localhost:3000/api/v1/user
 	  # Try below command on your terminal. if you get result true, it must be sccuess
 	  # $ curl -X PUT http://localhost:3000/api/v1/user/1 -d "window_id=test2;"
 	  #
-  desc "Register window_id to User"
     params do
      requires :user_id, type: Integer
      requires :window_id, type: String
@@ -18,15 +19,19 @@ class API < Grape::API
 	end
   end
 
+
+
+
   resource "users" do
-    # All User Info API: http://localhost:3000/api/v1/users
+
     desc "returns all users"
+    # All User Info API: http://localhost:3000/api/v1/users
     get do
       User.all
     end
 
-    # Users in the room API: http://localhost:3000/api/v1/users/:room_id
     desc "return a user in the room"
+    # Users in the room API: http://localhost:3000/api/v1/users/:room_id
     params do
       requires :room_id, type: Integer
     end
@@ -35,17 +40,45 @@ class API < Grape::API
     end
   end
 
+
+  resource "window_id" do
+  desc "Get User information with window_id and room_id"
+  # http://localhost:3000/api/v1/window_id/$window_id?room_id=$room_id
+  # サンプルは以下
+  # http://localhost:3000/api/v1/window_id/test2.xml?room_id=4
+    params do
+		requires :window_id, type: String
+		requires :room_id, type: Integer
+	end
+	get ':window_id' do
+		if params[:room_id]
+         User.where(window_id: params[:window_id]).where(room_id: params[:room_id])
+		else
+         User.where(window_id: params[:window_id])
+		end
+	end
+  end
+
+
+
+
   resource "rooms" do
-    # All Rooms Info API: http://localhost:3000/api/v1/rooms
+
 	  desc "returns all rooms"
+    # All Rooms Info API: http://localhost:3000/api/v1/rooms
 	  get do
 		  Room.all
 	  end
   end
 
+
+
+
+
   resource "room_full" do
-    # Room Full API: http://localhost:3000/api/v1/room_full/:room_id
+
 	  desc "return True when four member joined in the room"
+    # Room Full API: http://localhost:3000/api/v1/room_full/:room_id
     params do
       requires :room_id, type: Integer
     end
