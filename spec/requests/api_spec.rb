f4cb333abf1f63ec235e018282b361b0f3e005c9
xkumiyu <xkumiyu@gmail.com>
2016-01-03 10:51:29 +0900

    api test修正

--- spec/requests/api_spec.rb
+++ spec/requests/api_spec.rb
@@ -3,16 +3,20 @@ require 'rails_helper'
 RSpec.describe 'API', type: :request do
 
   describe "GET /api/v1/users/:room_id" do
-    it "roomのuser情報を返すこと" do
-      room = create(:room, male: 0, female: 1)
+    before :each do
+      @room = create(:room, male: 0, female: 1)
+      get "/api/v1/users/#{@room.id}.json"
+    end
 
-      get "/api/v1/users/#{room.id}.json"
+    it "正しくアクセスできること" do
       expect(response).to have_http_status(:success)
+    end
 
+    it "roomのuser情報を返すこと" do
       json = JSON.parse(response.body)
       expect(json.size).to eq 1
 
-      user = room.users.first
+      user = @room.users.first
       expect(json[0]['name']).to eq user.name
       expect(json[0]['gender']).to eq user.gender
     end
@@ -24,47 +28,98 @@ RSpec.describe 'API', type: :request do
   end
 
   describe "GET api/v1/room_full/:room_id" do
-    it "roomが満員のとき、trueを返すこと" do
-      room = create(:full_room)
+    context "roomが満員の場合" do
+      before :each do
+        room = create(:full_room)
+        get "/api/v1/room_full/#{room.id}.json"
+      end
 
-      get "/api/v1/room_full/#{room.id}.json"
-      expect(response).to have_http_status(:success)
+      it "正しくアクセスできること" do
+        expect(response).to have_http_status(:success)
+      end
 
-      json = JSON.parse(response.body)
-      expect(json['result']).to be_truthy
+      it "trueを返すこと" do
+        json = JSON.parse(response.body)
+        expect(json['result']).to be_truthy
+      end
     end
 
-    it "roomが満員でないとき、falseを返すこと" do
-      room = create(:room, male: 0, female: 1)
+    context "roomが満員でない場合" do
+      before :each do
+        room = create(:room, male: 0, female: 1)
+        get "/api/v1/room_full/#{room.id}.json"
+      end
 
-      get "/api/v1/room_full/#{room.id}.json"
-      expect(response).to have_http_status(:success)
+      it "正しくアクセスできること" do
+        expect(response).to have_http_status(:success)
+      end
 
-      json = JSON.parse(response.body)
-      expect(json['result']).to be_falsey
+      it "falseを返すこと" do
+        json = JSON.parse(response.body)
+        expect(json['result']).to be_falsey
+      end
     end
   end
 
   describe "PUT /api/v1/user/:user_id" do
-    it "userが存在する場合、trueを返し、windows_idを更新できること" do
-      user = create(:user)
+    context "userが存在する場合" do
+      before :each do
+        @user = create(:user)
+        put "/api/v1/user/#{@user.id}.json", {window_id: 'test2'}
+      end
 
-      put "/api/v1/user/#{user.id}.json", {window_id: 'test2'}
-      expect(response).to have_http_status(:success)
+      it "正しくアクセスできること" do
+        expect(response).to have_http_status(:success)
+      end
 
-      json = JSON.parse(response.body)
-      expect(json['result']).to be_truthy
+      it "trueを返すこと" do
+        json = JSON.parse(response.body)
+        expect(json['result']).to be_truthy
+      end
 
-      user.reload
-      expect(user.window_id).to eq 'test2'
+      it "window_idを更新できること" do
+        @user.reload
+        expect(@user.window_id).to eq 'test2'
+      end
     end
 
-    it "userが存在しない場合、falseを返すこと"
+    context "userが存在しない場合" do
+      before :each do
+        put "/api/v1/user/99.json", {window_id: 'test2'}
+      end
+
+      it "正しくアクセスできること" do
+        expect(response).to have_http_status(:success)
+      end
+
+      it "falseを返すこと" do
+        json = JSON.parse(response.body)
+        expect(json['result']).to be_falsey
+      end
+    end
   end
 
+  # user_idのほうがいいのでは？
   describe "DELETE /api/v1/window_id/:window_id?room_id=:room_id" do
+    before :each do
+      user = create(:room).users.first
+      user.update(window_id: 'test2')
+      delete "/api/v1/window_id/#{user.window_id}?room_id=#{user.room_id}.json"
+    end
+
+    it "正しくアクセスできること" do
+      expect(response).to have_http_status(:success)
+    end
+
+    it "trueを返すこと" do
+      json = JSON.parse(response.body)
+      expect(json['result']).to be_truthy
+    end
+
     it "userが削除されること"
-    # user_idのほうがいいのでは？
+    # it "userが削除されること" do
+    #   expect{ request }.to change(User, :count).by(-1)
+    # end
   end
 
 end
