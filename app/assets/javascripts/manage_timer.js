d84aedff81441955883efa6ea320300fcc2aa90d
mats0228 <m.hisa3153@gmail.com>
2016-01-10 00:34:54 +0900

    Update manage_timer.js
    ごめん検証できてないけど、これしないとマイナスなる気がする。

--- app/assets/javascripts/manage_timer.js
+++ app/assets/javascripts/manage_timer.js
@@ -17,6 +17,7 @@ function display_timer(){
     // m:残時間を60でわった値  s:残時間を60で割った余り
     var m = Math.floor(left_time / 60);
     var s = left_time % 60;
+    s = (s < 0) ? 0 : s; //マイナスならないよう修正
     var s_timer = ( (m > 0) ? (m + '分' + s + '秒') : (s + '秒') );
     $("#room_timer").html('<p id="timer_counter" > 終了まで' + s_timer + '</p>');
     setTimeout("display_timer()",1000); // 1sごと再帰呼び出し
